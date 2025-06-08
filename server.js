357db9e2cc6cbadef8706454b93f904022825162
bammite_prince <bammite_prince>
2025-06-08 01:27:22 +0000

    5eme

--- server.js
+++ server.js
@@ -116,7 +116,7 @@ app.use('/assets/uploads', (req, res, next) => {
 });
 
 // Start the server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
